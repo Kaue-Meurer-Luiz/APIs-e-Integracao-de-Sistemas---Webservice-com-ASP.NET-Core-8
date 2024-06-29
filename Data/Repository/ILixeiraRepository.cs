743eec1d3f3db71f7d647ed261bb13839ec06502
Kaue Meurer <kauemluiz@gmail.com>
2024-06-28 21:08:44 -0300

    Inplementacao Repository

---
+++ Data/Repository/ILixeiraRepository.cs
@@ -0,0 +1,16 @@
+﻿using Fiap.Api.Residuos.Models;
+
+namespace Fiap.Api.Residuos.Data.Repository
+{
+    public interface ILixeiraRepository
+    {
+        IEnumerable<LixeiraModel> GetAll();
+
+        LixeiraModel GetById(int id);
+        void Add (LixeiraModel lixeira);  
+        void Update (LixeiraModel lixeira);
+        void Delete (LixeiraModel lixeira);
+        
+
+    }
+}
